ec71499625165d37f03ab1cb1be77f19386d7514
hariprasaad89 <38822200+hariprasaad89@users.noreply.github.com>
2024-10-26 12:19:15 +0530

    Add files via upload

---
+++ Reversethenumber.py
@@ -0,0 +1,6 @@
+#Program to extract each digit from an integer in the reverse order
+
+number1 = input("Enter the number to reverse")
+print("number1 :", number1)
+number2=number1[::-1]
+print("Number got reversed : ",number2)
