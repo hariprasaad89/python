ec61069e2ff4ffc58f7bd1db5c0e7e9b2e225ce5
hariprasaad89 <38822200+hariprasaad89@users.noreply.github.com>
2024-10-29 21:27:38 +0530

    Add files via upload

---
+++ formating in print.py
@@ -0,0 +1,6 @@
+#formating in print
+totalmoney=int(input("Enter the total money spent"))
+quantity=int(input("Number of quantities"))
+price=int(input("total price"))
+
+print("I bought {} quantities of iphone of each {} price and total of {} rs".format(quantity,price,totalmoney))
