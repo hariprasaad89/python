ddd8c9097813fd9d3d3cc04827de15232b434b4d
hariprasaad89 <38822200+hariprasaad89@users.noreply.github.com>
2024-10-29 22:03:53 +0530

    Add files via upload

---
+++ sum the current number and the previous.py
@@ -0,0 +1,7 @@
+#sum the current number and the previous number
+givenNum=int(input("Enter the number to add with previous number"))
+print("currentNumber", givenNum)
+preNum=givenNum-1
+print ("previous number",preNum)
+summ=preNum+givenNum
+print("summ",summ)
